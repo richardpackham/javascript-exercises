efae4e9844d6cdf232e4c8f724cdb2f75c6322f2
rp-02 <rpackham75@gmail.com>
2023-06-14 12:36:54 -0400

    reverseString solution

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let length = string.length;
+    let position = length
+    let newStr = ""
+    for ( i = 0 ; i < length ; i++){
+        newStr = newStr + string.charAt(position-1);
+        position--;
+    }
+    return newStr
 };
 
 // Do not edit below this line
