930fd6f3cc83181210ff8db5c1af0052e2b0e7f3
rp-02 <rpackham75@gmail.com>
2023-06-14 12:50:01 -0400

    sumAll solution

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,11 @@
-const sumAll = function() {
-
+const sumAll = function(a,b) {
+    let loops = b-a + 1;
+    let sum = 0;
+    for (let i=0; i < loops; i++){
+        sum += a;
+        a++
+    }
+    return sum
 };
 
 // Do not edit below this line
