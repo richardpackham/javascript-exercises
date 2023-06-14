d7298c33d673c5f958c229896fc04eff3b1e026f
rp-02 <rpackham75@gmail.com>
2023-06-14 14:07:04 -0400

    ran tempConversion tests - passed

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,16 @@
-const convertToCelsius = function() {
+// fahrenheit to Celsius	x °F ≘ (x − 32) × 5/9 °C	
+// celsius to Fahrenheit x °C ≘ (x × 9/5 + 32) °F
+
+const convertToCelsius = function(fahrenheit) {
+  let celsius = ((fahrenheit-32)/9)*5
+  celsius = Math.round(celsius*10)/10
+  return celsius
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  let fahrenheit = ((celsius/5)*9) + 32
+  fahrenheit = Math.round(fahrenheit*10)/10
+  return fahrenheit
 };
 
 // Do not edit below this line
