5d0a01bedef057c36256573a2125bd65d7f61b36
rp-02 <rpackham75@gmail.com>
2023-06-14 13:49:55 -0400

    removeFromArray modified to pass all tests

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,7 +1,12 @@
-const removeFromArray = function(array,elementToRemove) {
+const removeFromArray = function(array,...elementsToRemove) {
+    for (const element in elementsToRemove){
     length = array.length;
-    index = (array.indexOf(elementToRemove));
+    index = (array.indexOf(elementsToRemove[element]));
+    if (index == -1){
+        continue;
+    }
     array.splice(index,1);
+    }
     return array
 };
 
