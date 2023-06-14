b2593fd70d9f53e3d9721d7d178d40b793a68a41
rp-02 <rpackham75@gmail.com>
2023-06-14 13:21:06 -0400

    repeatString tests passed

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,4 +1,7 @@
 const repeatString = function(str,times) {
+    if (times < 0) {
+        return "ERROR";
+    }
     let string = ""
     for (let i = 0; i < times ; i++){
         string = string + str
