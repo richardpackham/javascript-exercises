12c344b7f132ebd4fcb971a963d732b32e6cd44b
rp-02 <rpackham75@gmail.com>
2023-06-14 14:03:12 -0400

    handle non number & negative numbers

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,9 +1,14 @@
 const sumAll = function(a,b) {
-    let loops = b-a + 1;
+    if (typeof a !="number" || typeof b != "number" || a < 0 || b < 0){
+        return "ERROR";
+    }
+    let larger = Math.max(a,b);
+    let smaller = Math.min(a,b);
+    let loops = larger - smaller + 1;
     let sum = 0;
     for (let i=0; i < loops; i++){
-        sum += a;
-        a++
+        sum += smaller;
+        smaller++
     }
     return sum
 };
