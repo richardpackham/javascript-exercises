d78662d06500f24c95051cc0b48a78580c4264b6
rp-02 <rpackham75@gmail.com>
2023-06-16 15:54:30 -0400

    palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,18 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    let newString = "";
+    // string = string.replace(" ", ""); 
+    for (let i = 0; i < string.length; i++){
+        if ((string[i].toLowerCase() != string[i].toUpperCase()) || (/^\d$/.test(string[i]))){
+            newString = newString + string[i].toLowerCase()
+        }
+    }
+    let length = newString.length
+    for (let i = 0; i <length; i++){
+        if (newString[i] != newString[length-i-1]){
+            return false
+        }
+    }   
+    return true
 };
 
 // Do not edit below this line
