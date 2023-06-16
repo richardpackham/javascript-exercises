b6bfeee2f7da1d1897cec503285220f1bb867568
rp-02 <rpackham75@gmail.com>
2023-06-16 18:34:35 -0400

    fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,29 @@
-const fibonacci = function() {
-
+const fibonacci = function(ind) {
+    let int = parseInt(ind)
+    if (int < 0){
+        return "OOPS"
+    }
+    else if (int == 0){
+        return 0
+    }   
+    else if (int == 1){
+        return 1
+    }
+    else {
+        let fib = [0,1];
+        let fibPrev = fib.length-1;
+        let fibTwoPrev = fibPrev-1;
+        
+        for (let i=1; int > fibPrev; i++){
+            if ((fib[fibPrev] + fib[fibTwoPrev])== i ){
+                fib.push(i);
+            }
+            fibPrev = fib.length-1;
+            fibTwoPrev = fibPrev-1;
+        }
+        return fib[int]
+    }
 };
-
+fibonacci()
 // Do not edit below this line
 module.exports = fibonacci;
