a3b7dd0ecdbcc7782154c1cd0166bc64c9a229d3
rp-02 <rpackham75@gmail.com>
2023-06-16 14:53:22 -0400

    sum, multiply, power

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -6,20 +6,28 @@ const subtract = function(a,b) {
 	return a-b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	let total = 0;
+  array.forEach(item => total+= item);
+  return total;
 };
 
-const multiply = function() {
-
+const multiply = function(...array) {
+  let total = 1;
+  array.forEach(item => total *= item);
+  return total;
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+	return Math.pow(a,b)
 };
 
-const factorial = function() {
-	
+const factorial = function(number) {
+	let total = 1;
+  for (let i=0; i < number; i++){
+    total *= i;
+  }
+  return total
 };
 
 // Do not edit below this line
