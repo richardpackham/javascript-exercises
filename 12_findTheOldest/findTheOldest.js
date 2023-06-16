42ec29f0916fea58130486804f3fbe19f12ceacc
rp-02 <rpackham75@gmail.com>
2023-06-16 19:04:14 -0400

    find the oldest solution

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,18 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    max = 0
+    maxPerson = ""
+    people.forEach(person =>{
+        if (!("yearOfDeath" in person)){
+            person.yearOfDeath = new Date().getFullYear()
+        }
+        person.age = person.yearOfDeath - person.yearOfBirth
+        if (person.age > max) {
+            max = person.age;
+            maxPerson = person.name;
+        }
+    })
+    const pos = people.map(e => e.age).indexOf(max);
+    return people[pos]
 };
 
 // Do not edit below this line
